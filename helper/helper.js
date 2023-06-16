99f9b1599497d101d658b7cf7c04967f259264e0
Calvin Roberts <you@example.com>
2023-06-16 17:21:05 -0600

    removed unnecessary code

--- helper/helper.js
+++ helper/helper.js
@@ -1,52 +1,17 @@
-// const fs = require('fs');
-// const util = require('util');
-
-// // Promise of fs.readFile
-// const readFromFile = util.promisify(fs.readFile);
-
-// //use fs.writeFile to write content object as a JSON string to specified destination
-// const writeToFile = (destination, content) =>
-//   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-//     err ? console.error(err) : console.info(`\nData written to ${destination}`)
-//   );
-
-// //readAndAppend function here
-// //reads contents of file, parses it as JSON, appends new content to data, writes updated data in original file
-// const readAndAppend = (content, file) => {
-//   fs.readFile(file, 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       const parsedData = JSON.parse(data);
-//       parsedData.push(content);
-//       writeToFile(file, parsedData);
-//     }
-//   });
-// };
-
-// module.exports = { readFromFile, writeToFile, readAndAppend };
-
 const fs = require('fs');
 const util = require('util');
 
-// Promise version of fs.readFile
+// Promise of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
+
+//use fs.writeFile to write content object as a JSON string to specified destination
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
+
+//readAndAppend function here
+//reads contents of file, parses it as JSON, appends new content to data, writes updated data in original file
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
